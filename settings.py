a1dfadb0acd4854ef131a5c910aeca390a0c5638
TredonA <Tausti@umich.edu>
2018-12-24 18:34:29 -0500

    After working on the screen resolution, I decided to make all settings related to the screen its own class in a seperate file to help save space and enhance clarity.

--- settings.py
+++ settings.py
@@ -1,1 +1,9 @@
+class Settings():
+    # A class to store the settings for the game
+    def _init_(self):
+        # Initialize the game settings
+        # Screen Settings
+        self.screen_width(960)
+        self.screen_height(640)
+        self.bg_color(230, 230, 230)
 
