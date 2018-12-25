03b91624436d0ef5c134d91caa1d211bbeade9c5
TredonA <Tausti@umich.edu>
2018-12-24 21:02:14 -0500

    Added a new ship.py file which will include the new ship class implementation. Afterwards, I added the ship instance to the alien_invasion.py file so that the ship continuously reloads along with the rest of the screen.

--- AlienInvasion.py
+++ AlienInvasion.py
@@ -1,13 +1,18 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
+
 def run_game():
     # Initialize game and create a screen object
     pygame.init()
     ai_settings = Settings()
-    screen = pygame.display.set_mode((ai_settings.screen_width,
-                                      ai_settings.screen_height))
+
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    # Make ship
+    ship = Ship(screen)
     # Start the main loop of the game
     while True:
         #Watch for keyboard and mouse events
@@ -17,7 +22,8 @@ def run_game():
 
         # Redraw the screen with each loop pass, starting with the background
         # color
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
         # Make the most recently drawn screen visible
         pygame.display.flip()
 
